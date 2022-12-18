b6ae555d306b931bc175a4539536eac1a95a91e8
Tata <tvmezhankova@rambler.ru>
2022-12-18 17:23:44 +0300

    first commit

---
+++ Task66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения A и B.
+// Напишите программу, которая найдет сумму натуральных элементов в промежутке от A до B.
+
+
+Console.Write("Введите значение переменной а: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение переменной b: ");
+            int b = int.Parse(Console.ReadLine());
+            if (a > b)    Console.WriteLine("Ошибка! Переменная а > b"); 
+else{          
+            int i, sum = 0;
+            for (i = a; b >= i; i++)
+            sum += i;
+            Console.WriteLine("Сумма чисел равна: " + sum);
+}
+          
